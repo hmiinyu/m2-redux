1c762eddb77eb84c8e4909167eb15667b11683a3
hmiinyu <hmiinyu@gmail.com>
2019-06-19 20:44:03 +0800

    feat(): update the m2-redux version

--- dist/root.js
+++ dist/root.js
@@ -7,22 +7,90 @@ exports.Root = void 0;
 
 var _react = _interopRequireDefault(require("react"));
 
+var _propTypes = _interopRequireDefault(require("prop-types"));
+
 var _reactRedux = require("react-redux");
 
 var _m2React = require("m2-react");
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
-var Root = function Root(_ref) {
-  var routes = _ref.routes,
-      store = _ref.store,
-      routeType = _ref.routeType;
+function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
 
-  var _routes = (0, _m2React.renderRoutes)(routes, '/', routeType);
+function _objectWithoutProperties(source, excluded) { if (source == null) return {}; var target = _objectWithoutPropertiesLoose(source, excluded); var key, i; if (Object.getOwnPropertySymbols) { var sourceSymbolKeys = Object.getOwnPropertySymbols(source); for (i = 0; i < sourceSymbolKeys.length; i++) { key = sourceSymbolKeys[i]; if (excluded.indexOf(key) >= 0) continue; if (!Object.prototype.propertyIsEnumerable.call(source, key)) continue; target[key] = source[key]; } } return target; }
 
-  return _react["default"].createElement(_reactRedux.Provider, {
-    store: store
-  }, _routes);
-};
+function _objectWithoutPropertiesLoose(source, excluded) { if (source == null) return {}; var target = {}; var sourceKeys = Object.keys(source); var key, i; for (i = 0; i < sourceKeys.length; i++) { key = sourceKeys[i]; if (excluded.indexOf(key) >= 0) continue; target[key] = source[key]; } return target; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+
+function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
+
+function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+var Root =
+/*#__PURE__*/
+function (_React$Component) {
+  _inherits(Root, _React$Component);
+
+  function Root() {
+    _classCallCheck(this, Root);
+
+    return _possibleConstructorReturn(this, _getPrototypeOf(Root).apply(this, arguments));
+  }
+
+  _createClass(Root, [{
+    key: "componentDidMount",
+    value: function componentDidMount() {
+      var _this = this;
+
+      var checkIsAuth = this.props.checkIsAuth;
+      this.__authenticated = checkIsAuth();
+      this.props.store.subscribe(function () {
+        if (_this.__authenticated !== checkIsAuth()) {
+          _this.__authenticated = checkIsAuth();
+
+          _this.forceUpdate();
+        }
+      });
+    }
+  }, {
+    key: "render",
+    value: function render() {
+      var _this$props = this.props,
+          store = _this$props.store,
+          routes = _this$props.routes,
+          config = _objectWithoutProperties(_this$props, ["store", "routes"]);
+
+      var _routes = (0, _m2React.renderRoutes)(routes, '/', config);
+
+      return _react["default"].createElement(_reactRedux.Provider, {
+        store: store
+      }, _routes);
+    }
+  }]);
+
+  return Root;
+}(_react["default"].Component);
 
 exports.Root = Root;
+
+_defineProperty(Root, "propTypes", {
+  store: _propTypes["default"].object.isRequired,
+  routes: _propTypes["default"].array.isRequired,
+  routeType: _propTypes["default"].string,
+  checkIsAuth: _propTypes["default"].func,
+  redirectUrl: _propTypes["default"].string,
+  redirect404: _propTypes["default"].string
+});
--- src/root.js
+++ src/root.js
@@ -1,12 +1,37 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Provider } from 'react-redux';
 import { renderRoutes } from 'm2-react';
 
-export const Root = ({routes, store, routeType}) => {
-  const _routes = renderRoutes(routes, '/', routeType);
-  return (
-    <Provider store={store}>
-      {_routes}
-    </Provider>
-  );
-};
+export class Root extends React.Component {
+  static propTypes = {
+    store: PropTypes.object.isRequired,
+    routes: PropTypes.array.isRequired,
+    routeType: PropTypes.string,
+    checkIsAuth: PropTypes.func,
+    redirectUrl: PropTypes.string,
+    redirect404: PropTypes.string
+  };
+
+  componentDidMount() {
+    const { checkIsAuth } = this.props;
+    this.__authenticated = checkIsAuth();
+    this.props.store.subscribe(() => {
+      if (this.__authenticated !== checkIsAuth()) {
+        this.__authenticated = checkIsAuth();
+        this.forceUpdate();
+      }
+    });
+  }
+
+  render() {
+    const { store, routes, ...config } = this.props;
+    const _routes = renderRoutes(routes, '/', config);
+
+    return (
+      <Provider store={store}>
+        {_routes}
+      </Provider>
+    )
+  }
+}
